f4b78b6048ea09847d0702fffe36a49a2a214af8
Jaime González Fábregas <jaimegonzalezfabregas@gmail.com>
2021-03-19 07:36:46 +0100

    a
    Former-commit-id: ac6d097b8b910f2bdcc936649df6be1fdeaf4a58

--- 2dShooter/server.js
+++ 2dShooter/server.js
@@ -1,12 +1,15 @@
+var fs = require('fs');
+var url = require('url');
+var http = require('http');
+
 let server = http.createServer(function (req, res) {
     let directions = url.parse(req.url, true)
 
-    console.log("called", directions.href)
+    console.log("called", directions.href, "query", directions.query, "pathname", directions.pathname)
 
     if (directions.pathname == "/file") {
         console.log("file querry")
         fs.readFile(directions.query.dir, function (err, data) {
-            res.writeHead(200, { 'Content-Type': directions.query.fileType });
             res.write(data);
             console.log("   returning")
             return res.end();
@@ -16,7 +19,7 @@ let server = http.createServer(function (req, res) {
 
     if (directions.pathname == "/") {
         console.log("base page querry")
-        fs.readFile('/index.html', function (err, data) {
+        fs.readFile('index.html', function (err, data) {
             res.writeHead(200, { 'Content-Type': 'text/html' });
 
             res.write(data);
@@ -26,18 +29,27 @@ let server = http.createServer(function (req, res) {
     }
 
     if (directions.pathname == "/AJAX") {
-
-        if (directions.query.queryPurpose == "getMap") {
+        console.log("it is an ajax")
+        if (directions.query.queryPurpose == "getData") {
+            console.log("returning gameData")
             res.writeHead(200, { 'Content-Type': 'application/json' });
-            res.write(JSON.stringify({ "map": map }));
+            res.write(JSON.stringify({ "map": map, "teams": teams }));
             return res.end();
         }
         if (directions.query.queryPurpose == "signIn") {
             let success = "OK";
             if (directions.query.name in users) {
                 success = "taken name"
-            } else if (teams[directions.query.team]){
+            } else if (teams[directions.query.team].playerNum >= maxPlayersPerTeam) {
+                teams[directions.query.team].playerNum++
+                users[directions.query.name] = { 
+                    "pos": teams[directions.query.team].initialPos, 
+                    "health": 1, 
+                    "team": directions.query.team
+                }
 
+            } else {
+                success = "no more space on team"
             }
 
             res.writeHead(200, { 'Content-Type': 'text/plain' });
@@ -46,19 +58,24 @@ let server = http.createServer(function (req, res) {
         }
         if (directions.query.queryPurpose == "refresh") {
 
+            users[directions.query.name].pos = directions.query.pos
+
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.write(JSON.stringify(users));
+            return res.end();
         }
 
     }
 
-    if (directions.pathname == "/favicon.ico") {
-        console.log("favico querry")
-        fs.readFile('favico.ico', function (err, data) {
-            //res.writeHead(200, { 'Content-Type': 'image/vnd.microsoft.icon' });
-            res.write(data);
-            console.log("   returning")
-            return res.end();
-        });
-    }
+    // if (directions.pathname == "/favicon.ico") {
+    //     console.log("favico querry")
+    //     fs.readFile('favico.ico', function (err, data) {
+    //         //res.writeHead(200, { 'Content-Type': 'image/vnd.microsoft.icon' });
+    //         res.write(data);
+    //         console.log("   returning")
+    //         return res.end();
+    //     });
+    // }
 
 });
 server.listen(8000);
@@ -68,19 +85,22 @@ let NextId = 0;
 let users = {}
 let teams = {
     "a": {
-        "playerNum": 0
+        "playerNum": 0,
+        "initialPos": [1, 1]
     },
     "b": {
-        "playerNum": 0
+        "playerNum": 0,
+        "initialPos": [4, 4]
     },
 }
 let maxPlayersPerTeam = 6;
 
 let map = [
-    "######",
-    "#    #",
-    "#    #",
-    "#    #",
-    "#    #",
-    "######",
+    "#######################",
+    "#      #              #",
+    "#               #     #",
+    "#####  ##########     #",
+    "#   #   #  #    #     #",
+    "#          #          #",
+    "#######################",
 ]
