3ae9f3134e8cfb4201d1d1a275f8fb3e36489459
Jaime González Fábregas <jaimegonzalezfabregas@gmail.com>
2021-06-29 19:39:13 +0200

    startasdsa

--- PaNCaKEs/PaNCaKEs.js
+++ PaNCaKEs/PaNCaKEs.js
@@ -11,5 +11,36 @@ async function readSimbols(then) {
 }
 
 readSimbols((symbols) => {
-    console.log(symbols)
+    //console.log(symbols)
+    let words = fs.readFileSync('mostUsedWords.txt').toString().split("\n"); //Object.keys(JSON.parse(fs.readFileSync("words.json")))
+
+    for (let word in words) {
+        let temp = check(words[word], symbols, "", 0)
+        if (temp != 0) {
+            console.log(temp)
+        }
+    }
+
 })
+
+function check(word, sym, construction, depth) {
+    //console.log(word)
+
+    if (word.length == 0) return depth > 3 ? construction : 0;
+    for (let i = 0; i < sym.length; i++) {
+        const symbol = sym[i];
+        if (Caps(word.substring(0, symbol.length)) == Caps(symbol)) {
+            let a = check(word.substring(symbol.length), sym, construction + symbol, depth + 1)
+            if (a != 0) {
+                return a;
+            }
+        }
+
+    }
+    return 0
+}
+
+function Caps(str) {
+    //console.log(str)
+    return str.toUpperCase()
+}
